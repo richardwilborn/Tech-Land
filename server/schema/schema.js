60c1c3e74ee93558cf422aadba4977c4fae88529
valopezs <valeriabrilopez@gmail.com>
2022-11-28 19:04:01 -0600

    client type schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,27 @@
-const {} = require(''
+const { freelancer, clients } = require('../sampleData.js')
 
-const graphql = require()
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLScalarType, graphql } = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone:  { type: GraphQLString }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                
+            }
+        }
+    }
+});
