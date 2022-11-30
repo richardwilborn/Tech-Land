6309b9a703e190d19b959fc2319e3488066c70be
Bitaw <stickynotes5@yahoo.com>
2022-11-30 12:36:33 -0600

    Added sections for contact info, skills, and sample work in profile.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,35 @@
 import "./Styles/Home.css"
+import "./Styles/Profile.css"
 
 
 function Home(){
     return (<>
-    <div className="Home"><h1>Pebbles</h1></div>
+    <div>
+    <img class="profilePic" src="./public/michelleyeoh.jpg" alt= "profile Pic"></img>
+    <div className="Home">
+        <section> <h1>Name</h1>
+        <p>fillertext</p>
+        <h1>Github:</h1>
+        <p>fillertext</p>
+        <h1>LinkedIn</h1>
+        <p>fillerText</p>
+        <h1>Specialties:</h1>
+        <p>fillertext</p></section>
+    </div>
+
+    <section>
+        <div className="sampleBox">
+            <h3>Sample Work: </h3>
+            <p>Filler text here</p>
+            <p>Filler Text Here</p>
+        </div>
+    </section>
+
+    <div className='skillList'>
+        <h3>Skills:</h3>
+        <p>Filler Text here</p>
+    </div>
+    </div>
     </>
     );
 }
