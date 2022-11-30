d0188c30f22799faec76e16f73c317a5e6c53592
angeltried <angeltried@icloud.com>
2022-11-29 21:16:28 -0600

    added html code to components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,10 +3,91 @@ import React from "react";
 
 
 
+
 function Home(){
 
     return (<>
-
+    <div class="container">
+    <div class="heading">
+      <h1>Welcome to Tech Land where you can be a freelance web-developer. Earn top dollar for your
+        work. Create an account and get started today!!!
+      </h1>
+    </div>
+    <div class="row">
+      <div class="card">
+        <div class="card-header">
+          <h1>Front-end</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h1>Front-end</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h1>Full-stack</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h1>Back-end</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h1>Full-stack</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-header">
+          <h1>Front-end</h1>
+        </div>
+        <div class="card-body">
+          <ul>
+            <li>Readme.md:</li>
+            <li>Pay:</li>
+          </ul>
+          <a href="#" class="btn">Accept</a>
+        </div>
+      </div>
+    </div>
+  </div>
     </>
     );
 }
