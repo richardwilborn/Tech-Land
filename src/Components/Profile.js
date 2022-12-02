8b64ba1a7e4056d58d2e7c4f4927e30479caa93a
Bitaw <stickynotes5@yahoo.com>
2022-12-01 18:49:49 -0600

    Added more styling for boxes in Profile.css

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 
 function Home(){
-    return (<>    <div classname="profileContainer">
-        <img class="profilePic" src="./public/michelleyeoh.jpg" alt= "profile Pic"></img>
+    return (<>    <div className="profileContainer">
+        <img className="profilePic" src="./img/michelleyeoh.jpg" alt= "profile Pic"></img>
         <div className="Home">
-            <section> <h1>Name</h1>
+            <section className="contactInfo"> 
+            <h1>Name</h1>
             <p>fillertext</p>
             <h1>Github:</h1>
             <p>fillertext</p>
@@ -18,14 +19,18 @@ function Home(){
     
         <section>
             <div className="sampleBox">
+                <div class="cardHeader">
                 <h3>Sample Work: </h3>
+                </div>
                 <p>Filler text here</p>
                 <p>Filler Text Here</p>
             </div>
         </section>
     
         <div className='skillList'>
+            <div class="cardHeader">
             <h3>Skills:</h3>
+            </div>
             <p>Filler Text here</p>
         </div>
         </div>
