04509dcde48012c2693260bd53988610efdf32ac
Richard Wilborn <45742979+richardwilborn@users.noreply.github.com>
2022-12-05 18:55:06 -0600

    added basic model structure

---
+++ server/models/profile.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  github: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  linkedIn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+});
+  
+  const Profile: model('Profile', profileSchema);
+
+  module.exports = Profile;
