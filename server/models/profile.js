975ce540e299e1df9124fcdc3005e99216dbd2e7
valopezs <valeriabrilopez@gmail.com>
2022-12-06 10:53:20 -0600

    setup middleware to create and compare password

--- server/models/profile.js
+++ server/models/profile.js
@@ -25,6 +25,21 @@ const profileSchema = new Schema({
     match: [/.+@.+\..+/, 'Must use a valid email address'],
   },
 });
+
+// set up middleware to create password
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
   
 const Profile = model('Profile', profileSchema);
 
