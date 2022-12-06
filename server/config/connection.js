978f9d43f584ec2c64f5243ce828d1d19f19d1aa
valopezs <valeriabrilopez@gmail.com>
2022-12-05 21:21:41 -0600

    updating dependencies

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
     //Make sure port number is right
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:4000/use-mutation',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:4000/techLand',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
