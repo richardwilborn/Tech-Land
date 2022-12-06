849b3576b0c8860d41db4e1bd2158f9b203b1135
Mariana Hernandez-Chavez <64447806+Mariana21hch@users.noreply.github.com>
2022-12-05 21:23:39 -0600

    edit webpack file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,7 +29,25 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Contact Cards',
+        short_name: 'Contact',
+        description: 'Never forget your contacts!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
