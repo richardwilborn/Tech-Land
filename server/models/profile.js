1605269f5a8aa007311c009a18e9be3a2163a4c7
valopezs <valeriabrilopez@gmail.com>
2022-12-05 19:41:17 -0600

    created profile model

---
+++ server/models/profile.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  github: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  linkedIn: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must use a valid email address'],
+  },
+});
+  
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
